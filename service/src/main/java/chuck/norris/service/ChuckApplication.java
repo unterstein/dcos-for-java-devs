c5cf856bea157464d54ba94a4c381119b5919b01
Michael Simons <michael@simons.ac>
2017-05-22 14:54:44 +0200

    Some changes to make the app more bootiful (#1)
    * Remove usage of version for managed dependencies
    
    ...and also update Spring Boot ;)
    
    You don't have to specifiy neither the version of Spring Boot components nor the one of managed dependencies if you use the starter pom and it's BOM.
    
    * Use Spring Boots version of specifing Java version
    
    The maven spring boot plugin uses that property to set the versions.
    
    * Make it a SpringBootApplication
    
    * Let's inject the locale
    
    * Use Spring Boot standard way of configuring the datasource
    
    First of all there's a property for that and I wouldn't manually instantiate the database If i didn't have to.
    
    Then, all properties defined that way can be overwritten by environment properties as as shown. There's a thing call "relaxed" binding that allows to transparently use CamelCase, dashes or UPPER_AND_UNDERSCORES.
    
    That relaxed binding is available only for ConfigurationProperties, not @Value.
    
    Find this and more in http://springbootbuch.de ;)
    
    * Use configurationproperties for the „service-version“, too
    
    Think about prefixing it…
    
    * Fix „evil“ typo
    
    There was a „`“ in the URL
    
    * Add support for spring boot actuator endpoints
    
    Also: Allow anonymous access and adapt the path to upcoming Spring Boot 2 changes
    
    * Replace „manual“ health endpoint with a custom health indicator
    
    If it isn’t healthy, response code is 503, so it should work as expected.
    
    * Let’s constructor inject the jdbctemplate

--- service/src/main/java/chuck/norris/service/ChuckApplication.java
+++ service/src/main/java/chuck/norris/service/ChuckApplication.java
@@ -2,33 +2,47 @@ package chuck.norris.service;
 
 import chuck.norris.service.api.HealthResponse;
 import chuck.norris.service.api.JokeResponse;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import javax.sql.DataSource;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.UUID;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
-@EnableAutoConfiguration
-@ComponentScan
+
+@SpringBootApplication
 @RestController("/")
 public class ChuckApplication extends WebMvcConfigurerAdapter {
 
+    @Component
+    @ConfigurationProperties // (prefix = "chuck") you can also prefix that stuff
+    static class ChuckConfiguration {
+        /** stores the application version of this service */
+        private String serviceVersion;
+
+        public String getServiceVersion() {
+            return serviceVersion;
+        }
+
+        public void setServiceVersion(String serviceVersion) {
+            this.serviceVersion = serviceVersion;
+        }
+    }        
+    
     private final static List<String> acceptedLanguages = Arrays.asList("en", "de");
 
     // stores the current nodeId of this service - current implemented as uuid
@@ -40,49 +54,40 @@ public class ChuckApplication extends WebMvcConfigurerAdapter {
     // stores the information if this service should be return healthy or unhealthy
     private boolean healthy = true;
 
-    // stores the application version of this service
-    @Value("${SERVICE_VERSION:1}")
-    private String version;
-
-    // stores the mysql url to connect to
-    @Value("${MYSQL_URL:jdbc:mysql://localhost:3306/chuck?user=root&password=&useTimezone=true&serverTimezone=Europe/Berlin}")
-    private String mysqlUrl;
-
-    public ChuckApplication() {
+    private final ChuckConfiguration chuckConfiguration;
+    
+    private final JdbcTemplate template;
+   
+    public ChuckApplication(final ChuckConfiguration chuckConfiguration, final JdbcTemplate jdbcTemplate) {
+        this.chuckConfiguration = chuckConfiguration;
+        this.template = jdbcTemplate;
         nodeId = UUID.randomUUID().toString();
         hostAddress = getHostAddress();
     }
 
-    @Autowired
-    JdbcTemplate template;
+    @Bean
+    public HealthIndicator chucksHealthIndicator() {
+        return () -> (healthy ? Health.up() : Health.down()).build();
+    }
 
     @RequestMapping("/")
-    public JokeResponse randomJoke() {
-        String locale = LocaleContextHolder.getLocale().getLanguage();
-        if (!acceptedLanguages.contains(locale)) {
-            locale = acceptedLanguages.get(0);
+    public JokeResponse randomJoke(final Locale locale) {
+        String lang = locale.getLanguage();
+        if (!acceptedLanguages.contains(lang)) {
+            lang = acceptedLanguages.get(0);
         }
-        Map<String, Object> query = template.queryForMap("SELECT * FROM `jokes` WHERE lang=? ORDER BY RAND() LIMIT 0,1;", locale);
+        Map<String, Object> query = template.queryForMap("SELECT * FROM `jokes` WHERE lang=? ORDER BY RAND() LIMIT 0,1;", lang);
 
         return new JokeResponse(
             hostAddress,
-            locale,
+            lang,
             nodeId,
-            version,
+            chuckConfiguration.getServiceVersion(),
             query.get("joke").toString()
         );
     }
 
-    @RequestMapping("/health")
-    public HealthResponse healthy() {
-        if (healthy) {
-            return new HealthResponse(true);
-        } else {
-            throw new RuntimeException("meh!");
-        }
-    }
-
-    @RequestMapping(value = "/health`", method = RequestMethod.DELETE)
+    @RequestMapping(value = "/health", method = RequestMethod.DELETE)
     public HealthResponse toggleHealth() {
         healthy = false;
         return new HealthResponse(false);
@@ -91,12 +96,7 @@ public class ChuckApplication extends WebMvcConfigurerAdapter {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(ChuckApplication.class, args);
     }
-
-    @Bean
-    public DataSource dataSource() {
-        return new DriverManagerDataSource(mysqlUrl);
-    }
-
+   
     private String getHostAddress() {
         try {
             return InetAddress.getLocalHost().getHostAddress();
